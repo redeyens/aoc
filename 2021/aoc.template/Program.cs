2074ba3b942cc0d0c13ea9f7abc2ee1ad46b692c
redeyens <red.eye.ns@gmail.com>
2021-11-18 12:42:58 +0100

    Created proj template to reduce repetitive work

---
+++ 2021/aoc.template/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace aoc.template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string line in Input1)
+            {
+                Console.WriteLine(line);
+            }
+
+            foreach (string line in Input2)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("aoc.template completed.");
+        }
+
+        private static IEnumerable<string> Input1
+        {
+            get
+            {
+                return GetLinesFromResource("aoc.template.Input.Input1.txt");
+            }
+        }
+
+        private static IEnumerable<string> Input2
+        {
+            get
+            {
+                return GetLinesFromResource("aoc.template.Input.Input2.txt");
+            }
+        }
+
+        private static IEnumerable<string> GetLinesFromResource(string name)
+        {
+            using (Stream in1Stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(name))
+            {
+                using (TextReader in1Reader = new StreamReader(in1Stream))
+                {
+                    yield return in1Reader.ReadLine();
+                }
+            }
+        }
+    }
+}
