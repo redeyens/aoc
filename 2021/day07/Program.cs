8b1c5ef074c0a3c2a04aa10c4615a3b3e3ea86a5
redeyens <red.eye.ns@gmail.com>
2021-12-07 07:32:02 +0100

    2021 day 7 part 1

---
+++ 2021/day07/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] crabs = PuzzleInput.First().Split(',').Select(int.Parse).OrderBy(n => n).ToArray();
+            int median = 0;
+
+            median = GetMedian(crabs);
+
+            int fuelRq = crabs.Select(c => Math.Abs(c - median)).Sum();
+
+            Console.WriteLine(fuelRq);
+
+            Console.WriteLine("day07 completed.");
+        }
+
+        private static int GetMedian(int[] values)
+        {
+            int median;
+            if (values.Length % 2 == 0)
+            {
+                median = (int)Math.Round((values[values.Length / 2 - 1] + values[values.Length / 2]) / 2.0);
+            }
+            else
+            {
+                median = values[values.Length / 2 + 1];
+            }
+
+            return median;
+        }
+
+        private static IEnumerable<string> TestInput
+        {
+            get
+            {
+                return GetLinesFromResource("day07.Input.TestInput.txt");
+            }
+        }
+
+        private static IEnumerable<string> PuzzleInput
+        {
+            get
+            {
+                return GetLinesFromResource("day07.Input.PuzzleInput.txt");
+            }
+        }
+
+        private static IEnumerable<string> GetLinesFromResource(string name)
+        {
+            using (Stream inStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(name))
+            {
+                using (TextReader inReader = new StreamReader(inStream))
+                {
+                    string line;
+                    while ((line = inReader.ReadLine()) != null)
+                    {
+                        yield return line;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 2021/day07/README.md
@@ -0,0 +1,26 @@
+To install this template:
+
+    dotnet new -i .\day07\
+
+Use this file to save puzzle text for future reference.
+
+Use backticks for `inline code`.
+
+Use at least 4 spaces or one tab indent for block code:
+
+    line 1
+    line 2
+
+Use double star for **emphasis**.
+
+You can also combine inline code and emphasis **`like this`**.
+
+To create unordered list:
+
+- item 1
+- item2
+    - item 3
+
+Use relative links to reference puzzle input:
+
+[Get your puzzle input.](Input/PuzzleInput.txt)
