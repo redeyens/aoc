3c89a7b31cb292868144464db9420787f01c81a9
redeyens <red.eye.ns@gmail.com>
2021-12-08 22:16:54 +0100

    Awful transitional solution, but still works for this input

--- 2020/day19_at2/Parser.cs
+++ 2020/day19_at2/Parser.cs
@@ -19,7 +19,7 @@ namespace day19_at2
 
             foreach (var ruleString in grammarString.Split(Environment.NewLine))
             {
-                var rule = ParseRule(ruleString);
+                var rule = ParseRule(ruleString, ruleId => grammar[ruleId]);
 
                 grammar[rule.Key] = rule.Value;
             }
@@ -27,7 +27,7 @@ namespace day19_at2
             return new Parser(grammar);
         }
 
-        private static KeyValuePair<string, Rule> ParseRule(string rule)
+        private static KeyValuePair<string, Rule> ParseRule(string rule, Func<string, Rule> ruleLookup)
         {
             string[] ruleDef = rule.Split(':');
             var ruleId = ruleDef[0];
@@ -41,14 +41,14 @@ namespace day19_at2
                 options.Add(sequence);
             }
 
-            return new KeyValuePair<string, Rule>(ruleId, new Rule(options));
+            return new KeyValuePair<string, Rule>(ruleId, new Rule(options, ruleLookup));
         }
 
         internal void Update(string newOrUpdatedRules)
         {
             foreach (var ruleString in newOrUpdatedRules.Split(Environment.NewLine))
             {
-                var rule = ParseRule(ruleString);
+                var rule = ParseRule(ruleString, ruleId => grammar[ruleId]);
 
                 grammar[rule.Key] = rule.Value;
             }
@@ -56,52 +56,7 @@ namespace day19_at2
 
         internal bool TryParse(string message, string rule)
         {
-            return MatchMessage("0", message).Where(residual => string.IsNullOrEmpty(residual)).Any();
-        }
-
-        private IEnumerable<string> MatchMessage(string startingRule, string message)
-        {
-            Rule ruleOptions = null;
-
-            // if the grammar does not contain this rule then it must be literal
-            if(!grammar.TryGetValue(startingRule, out ruleOptions))
-            {
-                return MatchLiteral(startingRule, message);
-            }
-            else
-            {
-                return MatchOptions(grammar[startingRule].Options, message);
-            }
-        }
-
-        private IEnumerable<string> MatchLiteral(string literal, string message)
-        {
-            if(message.StartsWith(literal))
-            {
-                yield return message.Substring(literal.Length);
-            }
-        }
-        private IEnumerable<string> MatchOptions(List<List<string>> ruleOptions, string message)
-        {
-            if(ruleOptions.Count == 1)
-            {
-                return MatchSequence(ruleOptions[0], message);
-            }
-            else
-            {
-                return MatchSequence(ruleOptions[0], message).Concat(MatchOptions(new List<List<string>>(ruleOptions.Skip(1)), message));
-            }
-        }
-        private IEnumerable<string> MatchSequence(List<string> ruleSequence, string message)
-        {
-            if(ruleSequence.Count == 1)
-            {
-                return MatchMessage(ruleSequence[0], message);
-            }
-            else
-            {
-                return MatchMessage(ruleSequence[0], message).SelectMany(residual => MatchSequence(new List<string>(ruleSequence.Skip(1)), residual));
-            }
+            return grammar["0"].MatchMessage(message).Where(residual => string.IsNullOrEmpty(residual)).Any();
         }
     }
 }
--- 2020/day19_at2/Rule.cs
+++ 2020/day19_at2/Rule.cs
@@ -1,16 +1,66 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace day19_at2
 {
     internal class Rule
     {
         private List<List<string>> options;
+        private Func<string, Rule> ruleLookup;
 
-        public Rule(List<List<string>> options)
+        public Rule(List<List<string>> options, Func<string, Rule> ruleLookup)
         {
             this.options = options;
+            this.ruleLookup = ruleLookup;
         }
 
-        public List<List<string>> Options => options;
+        internal IEnumerable<string> MatchMessage(string message)
+        {
+            // we need to temporarily redefine literal untill we sort out parsing
+            // it will be the only entry in the only sequence that is not a number
+            IEnumerable<string> terminals = options.SelectMany(options => options);
+            if (terminals.Count() == 1 && terminals.Where(t => char.IsLetter(t[0])).Any())
+            {
+                return MatchLiteral(terminals.First(), message);
+            }
+            else
+            {
+                return MatchOptions(options, message);
+            }
+        }
+
+        private IEnumerable<string> MatchLiteral(string literal, string message)
+        {
+            if(message.StartsWith(literal))
+            {
+                yield return message.Substring(literal.Length);
+            }
+        }
+
+        private IEnumerable<string> MatchOptions(List<List<string>> ruleOptions, string message)
+        {
+            if(ruleOptions.Count == 1)
+            {
+                return MatchSequence(ruleOptions[0], message);
+            }
+            else
+            {
+                return MatchSequence(ruleOptions[0], message).Concat(MatchOptions(new List<List<string>>(ruleOptions.Skip(1)), message));
+            }
+        }
+
+        private IEnumerable<string> MatchSequence(List<string> ruleSequence, string message)
+        {
+            Rule firstRule = ruleLookup(ruleSequence[0]);
+            if(ruleSequence.Count == 1)
+            {
+                return firstRule.MatchMessage(message);
+            }
+            else
+            {
+                return firstRule.MatchMessage(message).SelectMany(residual => MatchSequence(new List<string>(ruleSequence.Skip(1)), residual));
+            }
+        }
     }
 }
