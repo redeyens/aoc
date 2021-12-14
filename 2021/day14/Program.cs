1212a6179c7cddbeb06a0763db0fc4336c26571c
redeyens <red.eye.ns@gmail.com>
2021-12-14 07:57:46 +0100

    2021 day 14 part 2 attempt 2

--- 2021/day14/Program.cs
+++ 2021/day14/Program.cs
@@ -9,37 +9,73 @@ namespace day14
     {
         static void Main(string[] args)
         {
-            var polymer = PuzzleInput.First().ToList();
-            var insertions = PuzzleInput.Skip(2).Select(i => i.Split(" -> ")).ToDictionary(p => p[0], p => p[1][0]);
-            var pendingInsertions = new Stack<(int pos, char element)>();
-            var pair = new char[2];
+            var polymer = GetPairs(PuzzleInput.First());
+            var insertions = PuzzleInput.Skip(2).Select(i => i.Split(" -> ")).Select(s => (pair:s[0], el:s[1][0])).ToList();
+            var pendingPairs = new List<(char first, char second, long cnt)>();
 
             for (int step = 0; step < 40; step++)
             {
-                for (int i = 1; i < polymer.Count; i++)
+                pendingPairs.Clear();
+                foreach (var ins in insertions)
                 {
-                    char insertedElement = default(char);
-                    pair[0] = polymer[i - 1];
-                    pair[1] = polymer[i];
+                    TouchEntry(polymer, ins.pair[0], ins.pair[1]);
 
-                    if(insertions.TryGetValue(new string(pair), out insertedElement))
-                    {
-                        pendingInsertions.Push((i, insertedElement));
-                    }
+                    long pairCount = polymer[ins.pair[0]][ins.pair[1]];
+                    pendingPairs.Add((ins.pair[0], ins.pair[1], -pairCount));
+                    pendingPairs.Add((ins.pair[0], ins.el, pairCount));
+                    pendingPairs.Add((ins.el, ins.pair[1], pairCount));
                 }
-
-                while (pendingInsertions.Count >0)
+                foreach (var pair in pendingPairs)
                 {
-                    var ins = pendingInsertions.Pop();
-                    polymer.Insert(ins.pos, ins.element);
+                    polymer[pair.first][pair.second] += pair.cnt;
                 }
             }
 
-            var sortedElements = polymer.GroupBy(c => c).Select(g => g.Count()).OrderBy(n => n).ToList();
+            var sortedElements = polymer.Values
+                .SelectMany(s => s.Select(e => (c:e.Key, cnt:e.Value)))
+                .GroupBy(e => e.c, e => e.cnt)
+                .Select(g => g.Sum())
+                .OrderBy(c => c)
+                .ToList();
 
             Console.WriteLine(sortedElements[sortedElements.Count - 1] - sortedElements[0]);
 
             Console.WriteLine("day14 completed.");
+        }
+
+        private static Dictionary<char,Dictionary<char,long>> GetPairs(string startingPolymer)
+        {
+            var res = new Dictionary<char,Dictionary<char,long>>();
+
+            res[default(char)] = new Dictionary<char, long>();
+            res[default(char)][startingPolymer[0]] = 1;
+
+            for (int i = 1; i < startingPolymer.Length; i++)
+            {
+                char firstChar = startingPolymer[i - 1];
+                char secondChar = startingPolymer[i];
+
+                TouchEntry(res, firstChar, secondChar);
+
+                res[firstChar][secondChar] += 1;
+            }
+
+            return res;
+        }
+
+        private static void TouchEntry(Dictionary<char, Dictionary<char, long>> polymer, char firstChar, char secondChar)
+        {
+            Dictionary<char, long> second;
+            if (!polymer.TryGetValue(firstChar, out second))
+            {
+                second = new Dictionary<char, long>();
+                polymer[firstChar] = second;
+            }
+
+            if (!second.TryGetValue(secondChar, out long cnt))
+            {
+                second[secondChar] = 0;
+            }
         }
 
         private static IEnumerable<string> TestInput
