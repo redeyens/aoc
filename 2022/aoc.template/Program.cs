cd66f29f98c1d80784fdf6af0be772004b84feaa
Dusko Mirkovic <red.eye.ns@gmail.com>
2022-11-30 19:40:12 +0100

    Updated template for .net6

---
+++ 2022/aoc.template/Program.cs
@@ -0,0 +1,26 @@
+﻿foreach (string line in TestInput())
+{
+    Console.WriteLine(line);
+}
+
+foreach (string line in PuzzleInput())
+{
+    Console.WriteLine(line);
+}
+
+Console.WriteLine("aoc.template completed.");
+
+static IEnumerable<string> TestInput() => GetLinesFromResource("aoc.template.Input.TestInput.txt");
+static IEnumerable<string> PuzzleInput() => GetLinesFromResource("aoc.template.Input.PuzzleInput.txt");
+
+static IEnumerable<string> GetLinesFromResource(string name)
+{
+    using Stream inStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(name) ?? throw new InvalidOperationException("Could not open input stream!");
+    using TextReader inReader = new StreamReader(inStream);
+
+    string? line;
+    while ((line = inReader.ReadLine()) != null)
+    {
+        yield return line;
+    }
+}
