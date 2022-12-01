37c5baf1ad4c2b465fe9623c6eda141b5af2c74e
Dusko Mirkovic <red.eye.ns@gmail.com>
2022-12-01 20:12:24 +0100

    day01 part 1

---
+++ 2022/day01/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine(GetCalorieLists(PuzzleInput()).Max(l => l.Sum()));
+
+Console.WriteLine("day01 completed.");
+
+static IEnumerable<List<int>> GetCalorieLists(IEnumerable<string> source)
+{
+    List<int> result = new();
+
+    foreach (var line in source)
+    {
+        if (string.IsNullOrEmpty(line))
+        {
+            yield return result;
+            result = new();
+        }
+        else
+        {
+            result.Add(int.Parse(line));
+        }
+    }
+    yield return result;
+}
+
+static IEnumerable<string> TestInput() => GetLinesFromResource("day01.Input.TestInput.txt");
+static IEnumerable<string> PuzzleInput() => GetLinesFromResource("day01.Input.PuzzleInput.txt");
+
+static IEnumerable<string> GetLinesFromResource(string name)
+{
+    using Stream inStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(name) ?? throw new InvalidOperationException("Could not open input stream!");
+    using TextReader inReader = new StreamReader(inStream);
+
+    string? line;
+    while ((line = inReader.ReadLine()) != null)
+    {
+        yield return line;
+    }
+}
