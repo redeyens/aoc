70e35b8e07e59d0754fb47e94a4d01edde94ad4d
Dusko Mirkovic <red.eye.ns@gmail.com>
2022-12-02 06:36:32 +0100

    day02 part 1

---
+++ 2022/day02/Program.cs
@@ -0,0 +1,31 @@
+﻿int[][] scores = new int[][] { new int[] { 3, 6, 0 }, new int[] { 0, 3, 6 }, new int[] { 6, 0, 3 } };
+
+int score = 0;
+
+foreach (string line in PuzzleInput())
+{
+    var play = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int moveScore = play[1][0] - 'X' + 1;
+    int outcomeScore = scores[play[0][0] - 'A'][play[1][0] - 'X'];
+    Console.WriteLine($"{moveScore} + {outcomeScore} = {moveScore + outcomeScore}");
+    score += moveScore + outcomeScore;
+}
+
+Console.WriteLine(score);
+
+Console.WriteLine("day02 completed.");
+
+static IEnumerable<string> TestInput() => GetLinesFromResource("day02.Input.TestInput.txt");
+static IEnumerable<string> PuzzleInput() => GetLinesFromResource("day02.Input.PuzzleInput.txt");
+
+static IEnumerable<string> GetLinesFromResource(string name)
+{
+    using Stream inStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(name) ?? throw new InvalidOperationException("Could not open input stream!");
+    using TextReader inReader = new StreamReader(inStream);
+
+    string? line;
+    while ((line = inReader.ReadLine()) != null)
+    {
+        yield return line;
+    }
+}
