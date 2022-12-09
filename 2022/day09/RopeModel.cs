f753aa7933bd7a400c78cab211c47bc1445399bf
redeyens <red.eye.ns@gmail.com>
2022-12-09 08:39:45 +0100

    day09 part 2

--- 2022/day09/RopeModel.cs
+++ 2022/day09/RopeModel.cs
@@ -1,36 +1,38 @@
 
 class RopeModel
 {
-    private (int x, int y) head = new();
-    private (int x, int y) tail = new();
+    private (int x, int y)[] knots = new (int x, int y)[10];
     private readonly HashSet<(int x, int y)> tailHistory = new();
 
     internal void MoveHead(char dir, int steps)
     {
         for (int i = 0; i < steps; i++)
         {
-            head = dir switch
+            knots[0] = dir switch
             {
-                'U' => (head.x, head.y + 1),
-                'D' => (head.x, head.y - 1),
-                'R' => (head.x + 1, head.y),
-                'L' => (head.x - 1, head.y),
+                'U' => (knots[0].x, knots[0].y + 1),
+                'D' => (knots[0].x, knots[0].y - 1),
+                'R' => (knots[0].x + 1, knots[0].y),
+                'L' => (knots[0].x - 1, knots[0].y),
                 _ => throw new ArgumentException($"Unknown direction '{dir}'.")
             };
 
-            var dx = head.x - tail.x;
-            var dy = head.y - tail.y;
-
-            if (Math.Abs(dx) > 1)
+            for (int j = 1; j < knots.Length; j++)
             {
-                tail = (tail.x + Math.Sign(dx), tail.y + Math.Sign(dy));
-            }
-            if (Math.Abs(dy) > 1)
-            {
-                tail = (tail.x + Math.Sign(dx), tail.y + Math.Sign(dy));
+                var dx = knots[j - 1].x - knots[j].x;
+                var dy = knots[j - 1].y - knots[j].y;
+
+                if (Math.Abs(dx) > 1)
+                {
+                    knots[j] = (knots[j].x + Math.Sign(dx), knots[j].y + Math.Sign(dy));
+                }
+                else if (Math.Abs(dy) > 1)
+                {
+                    knots[j] = (knots[j].x + Math.Sign(dx), knots[j].y + Math.Sign(dy));
+                }
             }
 
-            tailHistory.Add(tail);
+            tailHistory.Add(knots[knots.Length - 1]);
         }
     }
 
