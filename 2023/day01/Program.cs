d235bf2bf569cb38ac20ffb4c6c58f2db669b1b6
redeyens <54171005+redeyens@users.noreply.github.com>
2023-12-01 08:20:08 +0100

    2023 day01 part 2

--- 2023/day01/Program.cs
+++ 2023/day01/Program.cs
@@ -1,9 +1,18 @@
-﻿using System.Linq;
-
-long result = 0;
+﻿long result = 0;
 foreach (string line in PuzzleInput())
 {
-    var digits = line.Where(c => char.IsDigit(c)).ToList();
+    var l = line;
+    l = l.Replace("one", "o1ne");
+    l = l.Replace("two", "t2wo");
+    l = l.Replace("three", "th3ree");
+    l = l.Replace("four", "fo4ur");
+    l = l.Replace("five", "fi5ve");
+    l = l.Replace("six", "si6x");
+    l = l.Replace("seven", "se7ven");
+    l = l.Replace("eight", "ei8ght");
+    l = l.Replace("nine", "ni9ne");
+
+    var digits = l.Where(c => char.IsDigit(c)).ToList();
     result += (digits.FirstOrDefault() - '0') * 10 + (digits.LastOrDefault() - '0');
 }
 Console.WriteLine(result);
