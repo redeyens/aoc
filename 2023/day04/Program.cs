8875c7e7d549ffd30530f3c2bcd5e729ab251f79
redeyens <54171005+redeyens@users.noreply.github.com>
2023-12-04 06:53:22 +0100

    2023 day 04 part 1

---
+++ 2023/day04/Program.cs
@@ -0,0 +1,34 @@
+﻿var totalVal = 0;
+
+foreach (string line in PuzzleInput())
+{
+    var splitOpt = StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries;
+    var numbers = line.Split(":", splitOpt)[1].Split("|", splitOpt);
+    var winningNumbers = numbers[0].Split(" ", splitOpt).Select(s => int.Parse(s));
+    var myNumbers = numbers[1].Split(" ", splitOpt).Select(s => int.Parse(s));
+    var val = 0;
+    foreach (var n in myNumbers.Intersect(winningNumbers))
+    {
+        val = val == 0 ? 1 : val << 1;
+
+    }
+    totalVal += val;
+}
+
+Console.WriteLine(totalVal);
+Console.WriteLine("day04 completed.");
+
+static IEnumerable<string> TestInput() => GetLinesFromResource("day04.Input.TestInput.txt");
+static IEnumerable<string> PuzzleInput() => GetLinesFromResource("day04.Input.PuzzleInput.txt");
+
+static IEnumerable<string> GetLinesFromResource(string name)
+{
+    using Stream inStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(name) ?? throw new InvalidOperationException("Could not open input stream!");
+    using TextReader inReader = new StreamReader(inStream);
+
+    string? line;
+    while ((line = inReader.ReadLine()) != null)
+    {
+        yield return line;
+    }
+}
