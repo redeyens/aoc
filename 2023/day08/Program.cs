e545ff795a8a8da320cba1ef5e15a1ed5a677503
redeyens <54171005+redeyens@users.noreply.github.com>
2023-12-08 07:53:53 +0100

    2023 day 08 part 2

--- 2023/day08/Program.cs
+++ 2023/day08/Program.cs
@@ -5,15 +5,22 @@ var map = input.Skip(2)
     .Select(arr => (k: arr[0], v: arr[1].Replace("(", string.Empty).Replace(")", string.Empty).Split(",", StringSplitOptions.TrimEntries)))
     .ToDictionary(x => x.k, x => x.v);
 
-var currentElement = "AAA";
+var currentElements = map.Keys.Where(k => k.EndsWith("A")).ToList();
 var steps = 0;
+var pathLen = new Dictionary<string, int>();
 
-while (!currentElement.Equals("ZZZ"))
+while (currentElements.Any())
 {
-    currentElement = map[currentElement][instructions[steps++ % instructions.Count]];
+    foreach (var e in currentElements.Where(e => e[2] == 'Z').ToList())
+    {
+        pathLen[e] = steps;
+        currentElements.Remove(e);
+    }
+    currentElements = currentElements.Select(currentElement => map[currentElement][instructions[steps % instructions.Count]]).ToList();
+    steps++;
 }
 
-Console.WriteLine(steps);
+Console.WriteLine(pathLen.Values.Aggregate(1L, (a, n) => Lcm(a, n)));
 Console.WriteLine("day08 completed.");
 
 static IEnumerable<string> TestInput() => GetLinesFromResource("day08.Input.TestInput.txt");
@@ -29,4 +36,18 @@ static IEnumerable<string> GetLinesFromResource(string name)
     {
         yield return line;
     }
+}
+
+static long Gcd(long a, long b)
+{
+    while (b != 0)
+    {
+        (a, b) = (b, a % b);
+    }
+    return a;
+}
+
+static long Lcm(long a, long b)
+{
+    return a * b / Gcd(a, b);
 }
