b382a28c854c0510cd2520c14cc4425d747f421f
redeyens <54171005+redeyens@users.noreply.github.com>
2023-12-11 08:10:32 +0100

    2023 day 11 part 2

--- 2023/day11/Program.cs
+++ 2023/day11/Program.cs
@@ -1,24 +1,22 @@
-﻿using System.Data;
-using System.Security.AccessControl;
+﻿
+var galaxies = PuzzleInput().SelectMany((l, row) => l.Select((c, col) => (c, coord: (row: (long)row, col: (long)col)))).Where(g => g.c == '#').Select(g => g.coord).OrderBy(g => g.row).ToList();
 
-var galaxies = PuzzleInput().SelectMany((l, row) => l.Select((c, col) => (c, coord: (row: row, col: col)))).Where(g => g.c == '#').Select(g => g.coord).OrderBy(g => g.row).ToList();
-
-var prevNonEmptyRow = 0;
-var currentInflation = 0;
+var prevNonEmptyRow = 0L;
+var currentInflation = 0L;
 for (int i = 0; i < galaxies.Count; i++)
 {
-    var localInflation = (galaxies[i].row - prevNonEmptyRow > 1) ? galaxies[i].row - prevNonEmptyRow - 1 : 0;
+    var localInflation = (galaxies[i].row - prevNonEmptyRow > 1) ? (galaxies[i].row - prevNonEmptyRow - 1) * 999999 : 0;
     currentInflation += localInflation;
     prevNonEmptyRow = galaxies[i].row;
     galaxies[i] = (galaxies[i].row + currentInflation, galaxies[i].col);
 }
 
 galaxies = galaxies.OrderBy(g => g.col).ToList();
-var prevNonEmptyCol = 0;
-currentInflation = 0;
+var prevNonEmptyCol = 0L;
+currentInflation = 0L;
 for (int i = 0; i < galaxies.Count; i++)
 {
-    var localInflation = (galaxies[i].col - prevNonEmptyCol > 1) ? galaxies[i].col - prevNonEmptyCol - 1 : 0;
+    var localInflation = (galaxies[i].col - prevNonEmptyCol > 1) ? (galaxies[i].col - prevNonEmptyCol - 1) * 999999 : 0;
     currentInflation += localInflation;
     prevNonEmptyCol = galaxies[i].col;
     galaxies[i] = (galaxies[i].row, galaxies[i].col + currentInflation);
@@ -31,7 +29,7 @@ Console.WriteLine("day11 completed.");
 static IEnumerable<string> TestInput() => GetLinesFromResource("day11.Input.TestInput.txt");
 static IEnumerable<string> PuzzleInput() => GetLinesFromResource("day11.Input.PuzzleInput.txt");
 
-IEnumerable<((int row, int col), (int row, int col))> GetAllPairs(List<(int row, int col)> galaxies)
+IEnumerable<((long row, long col), (long row, long col))> GetAllPairs(List<(long row, long col)> galaxies)
 {
     for (int i = 0; i < galaxies.Count - 1; i++)
     {
