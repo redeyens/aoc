69c907e3160133022e39eb99ec72318ca1f4bfb2
redeyens <54171005+redeyens@users.noreply.github.com>
2023-12-11 23:42:24 +0100

    2023 day 10 part 1 redesign

--- 2023/day10/Program.cs
+++ 2023/day10/Program.cs
@@ -1,71 +1,55 @@
-﻿var segmentMap = new Dictionary<(int row, int col), HashSet<(int row, int col)>>();
+﻿var segmentMap = new Dictionary<(int row, int col), IEnumerable<(int dr, int dc)>>();
 var start = (row: -1, col: -1);
-var bigRowCnt = 0;
+var row = 0;
 
 foreach (string line in PuzzleInput())
 {
     var startIndex = line.IndexOf('S');
     if (startIndex >= 0)
     {
-        start = (row: bigRowCnt, col: startIndex);
+        start = (row: row, col: startIndex);
     }
-    var pipeSegments = line.SelectMany((c, i) => GetPipeSegment(c, i, bigRowCnt)).ToList();
+    var pipeSegments = line.Select((c, col) => (pos: (row, col), steps: GetSteps(c))).Where(x => x.steps.Any());
     foreach (var segment in pipeSegments)
     {
-        HashSet<(int row, int col)> set;
-        if (!segmentMap.TryGetValue(segment.Item1, out set))
-        {
-            set = new HashSet<(int row, int col)>();
-            segmentMap[segment.Item1] = set;
-        }
-        set.Add(segment.Item2);
-
-        if (!segmentMap.TryGetValue(segment.Item2, out set))
-        {
-            set = new HashSet<(int row, int col)>();
-            segmentMap[segment.Item2] = set;
-        }
-        set.Add(segment.Item1);
+        segmentMap[segment.pos] = segment.steps;
     }
-    bigRowCnt++;
+    row++;
 }
 
-var possibleStart = new (int, int)[] { (2 * start.row, 2 * start.col + 1), (2 * start.row + 1, 2 * start.col + 2), (2 * start.row + 2, 2 * start.col + 1), (2 * start.row + 1, 2 * start.col) };
+var possibleStart = new (int row, int col)[] { (start.row - 1, start.col), (start.row + 1, start.col), (start.row, start.col - 1), (start.row, start.col + 1) };
 
-var horizon = new HashSet<(int row, int col)>(possibleStart.Where(x => segmentMap.ContainsKey(x)));
-var visited = new HashSet<(int row, int col)>();
-var steps = 0;
-
-while (horizon.Count > 1)
+var horizon = new Queue<(int row, int col)>(possibleStart.Where(x => segmentMap.ContainsKey(x) && segmentMap[x].Select(d => (x.row + d.dr, x.col + d.dc)).Any(x => start == x)));
+var visited = new HashSet<(int row, int col)>
 {
-    foreach (var item in horizon)
+    start
+};
+
+while (horizon.Count > 0)
+{
+    var node = horizon.Dequeue();
+    foreach (var item in segmentMap[node].Select(d => (node.row + d.dr, node.col + d.dc)).Except(visited))
     {
-        visited.Add(item);
+        horizon.Enqueue(item);
     }
-    var next = horizon.SelectMany(x => segmentMap[x].Except(visited)).ToList();
-    horizon.Clear();
-    foreach (var item in next)
-    {
-        horizon.Add(item);
-    }
-    steps++;
+    visited.Add(node);
 }
 
-System.Console.WriteLine(steps);
+Console.WriteLine(visited.Count / 2);
 Console.WriteLine("day10 completed.");
 
 static IEnumerable<string> TestInput() => GetLinesFromResource("day10.Input.TestInput.txt");
 static IEnumerable<string> PuzzleInput() => GetLinesFromResource("day10.Input.PuzzleInput.txt");
 
-static IEnumerable<((int, int), (int, int))> GetPipeSegment(char c, int i, int bigRowCnt) => c switch
+static IEnumerable<(int, int)> GetSteps(char c) => c switch
 {
-    '|' => new ((int, int), (int, int))[] { ((2 * bigRowCnt, 2 * i + 1), (2 * (bigRowCnt + 1), 2 * i + 1)) },
-    '-' => new ((int, int), (int, int))[] { ((2 * bigRowCnt + 1, 2 * i), (2 * bigRowCnt + 1, 2 * (i + 1))) },
-    'L' => new ((int, int), (int, int))[] { ((2 * bigRowCnt, 2 * i + 1), (2 * bigRowCnt + 1, 2 * (i + 1))) },
-    'J' => new ((int, int), (int, int))[] { ((2 * bigRowCnt, 2 * i + 1), (2 * bigRowCnt + 1, 2 * i)) },
-    '7' => new ((int, int), (int, int))[] { ((2 * bigRowCnt + 1, 2 * i), (2 * (bigRowCnt + 1), 2 * i + 1)) },
-    'F' => new ((int, int), (int, int))[] { ((2 * (bigRowCnt + 1), 2 * i + 1), (2 * bigRowCnt + 1, 2 * (i + 1))) },
-    _ => Enumerable.Empty<((int, int), (int, int))>()
+    '|' => new (int, int)[] { (-1, 0), (1, 0) },
+    '-' => new (int, int)[] { (0, -1), (0, 1) },
+    'L' => new (int, int)[] { (-1, 0), (0, 1) },
+    'J' => new (int, int)[] { (-1, 0), (0, -1) },
+    '7' => new (int, int)[] { (0, -1), (1, 0) },
+    'F' => new (int, int)[] { (1, 0), (0, 1) },
+    _ => Enumerable.Empty<(int, int)>()
 };
 
 static IEnumerable<string> GetLinesFromResource(string name)
