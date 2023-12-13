6cf569dc83d998c25f8fd5b3fcb6bad311907d77
redeyens <54171005+redeyens@users.noreply.github.com>
2023-12-13 22:22:20 +0100

    2023 day 13 part 2

--- 2023/day13/Program.cs
+++ 2023/day13/Program.cs
@@ -4,19 +4,18 @@ var sum = 0;
 foreach (var pattern in patterns)
 {
     var lines = pattern.Split(Environment.NewLine, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-    var mirrorCol = Enumerable.Range(1, lines[0].Length - 1).ToList();
+    var mirrorColCandidates = Enumerable.Range(1, lines[0].Length - 1).ToDictionary(x => x, y => 0);
     foreach (var line in lines)
     {
-        mirrorCol = mirrorCol.Where(c => IsThisMirrorLine(line, c)).ToList();
-        if (!mirrorCol.Any())
+        foreach (var c in mirrorColCandidates.Keys)
         {
-            break;
+            mirrorColCandidates[c] += CountDiffsMirror(line, c);
         }
     }
-    sum += mirrorCol.FirstOrDefault();
+    sum += mirrorColCandidates.Where(x => x.Value == 1).Select(x => x.Key).FirstOrDefault();
 
     var mirrorRow = Enumerable.Range(1, lines.Length - 1);
-    sum += 100 * mirrorRow.Where(r => IsThisMirror(lines, r)).FirstOrDefault();
+    sum += 100 * mirrorRow.Select(r => (r, d: ContDiffsMirrorLines(lines, r))).Where(x => x.d == 1).Select(x => x.r).FirstOrDefault();
 }
 
 System.Console.WriteLine(sum);
@@ -25,28 +24,33 @@ Console.WriteLine("day13 completed.");
 static string TestInput() => GetLinesFromResource("day13.Input.TestInput.txt");
 static string PuzzleInput() => GetLinesFromResource("day13.Input.PuzzleInput.txt");
 
-static bool IsThisMirrorLine(string line, int left)
+static int CountDiffsMirror(string line, int left)
 {
+    int diffs = 0;
     for (int i = 1; left - i >= 0 && i + left - 1 < line.Length; i++)
     {
         if (line[left - i] != line[left + i - 1])
         {
-            return false;
+            diffs++;
         }
     }
-    return true;
+    return diffs;
 }
 
-static bool IsThisMirror(string[] lines, int top)
+static int ContDiffsMirrorLines(string[] lines, int top)
 {
+    int diffs = 0;
     for (int i = 1; top - i >= 0 && i + top - 1 < lines.Length; i++)
     {
-        if (!lines[top - i].Equals(lines[top + i - 1]))
+        for (int j = 0; j < lines[top - i].Length; j++)
         {
-            return false;
+            if (lines[top - i][j] != lines[top + i - 1][j])
+            {
+                diffs++;
+            }
         }
     }
-    return true;
+    return diffs;
 }
 
 static string GetLinesFromResource(string name)
