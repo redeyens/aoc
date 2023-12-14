1843796362ac1ee253b58f27e717d7ab30d0ac5c
redeyens <54171005+redeyens@users.noreply.github.com>
2023-12-14 21:29:05 +0100

    2023 day 14 prep for part 2

--- 2023/day14/Program.cs
+++ 2023/day14/Program.cs
@@ -1,34 +1,35 @@
-﻿var rowSupport = new int[0];
+﻿var rowSupport = Array.Empty<int>();
 var input = PuzzleInput().ToList();
-var totalLoad = 0;
-var row = 0;
-foreach (string line in input)
-{
-    if (rowSupport.Length == 0)
-    {
-        rowSupport = new int[line.Length];
-    }
-    for (int i = 0; i < line.Length; i++)
-    {
-        if (line[i] == 'O')
-        {
-            totalLoad += input.Count - rowSupport[i];
-            rowSupport[i]++;
-        }
-        if (line[i] == '#')
-        {
-            rowSupport[i] = row + 1;
-        }
-    }
+var stones = input.SelectMany((l, r) => l.Select((x, c) => (m: x, r, c)).Where(x => x.m != '.'));
+stones = TiltNorth(stones);
 
-    row++;
-}
+var totalLoad = stones.Where(x => x.m == 'O').Sum(x => input.Count - x.r);
 
 Console.WriteLine(totalLoad);
 Console.WriteLine("day14 completed.");
 
 static IEnumerable<string> TestInput() => GetLinesFromResource("day14.Input.TestInput.txt");
 static IEnumerable<string> PuzzleInput() => GetLinesFromResource("day14.Input.PuzzleInput.txt");
+
+IEnumerable<(char m, int r, int c)> TiltNorth(IEnumerable<(char m, int r, int c)> stones)
+{
+    foreach (var columnGroup in stones.GroupBy(x => x.c))
+    {
+        var currentRow = 0;
+        foreach (var stone in columnGroup.OrderBy(x => x.r))
+        {
+            if (stone.m == 'O')
+            {
+                yield return (stone.m, currentRow++, stone.c);
+            }
+            else
+            {
+                currentRow = stone.r + 1;
+                yield return stone;
+            }
+        }
+    }
+}
 
 static IEnumerable<string> GetLinesFromResource(string name)
 {
