5b5a070c7a0f7ad8ab686d28e37fb521a45d1e62
redeyens <54171005+redeyens@users.noreply.github.com>
2023-12-15 07:24:49 +0100

    2023 day 15 part 2

--- 2023/day15/Program.cs
+++ 2023/day15/Program.cs
@@ -1,10 +1,71 @@
 ﻿var total = 0;
+var boxes = new Dictionary<int, LinkedList<(string label, int f)>>();
 foreach (string line in PuzzleInput())
 {
-    total += line.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(x => GetGash(x)).Sum();
+    foreach (var cmd in line.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+    {
+        var actionIndex = cmd.IndexOf('=');
+        var label = string.Empty;
+        int f = 0;
+        var action = '=';
+        if (actionIndex >= 0)
+        {
+            var parts = cmd.Split('=');
+            label = parts[0];
+            f = int.Parse(parts[1]);
+        }
+        else
+        {
+            action = '-';
+            label = cmd.Split('-')[0];
+        }
+
+        LinkedList<(string label, int f)> list;
+        var key = GetHash(label);
+        if (!boxes.TryGetValue(key, out list))
+        {
+            list = new LinkedList<(string label, int f)>();
+            boxes[key] = list;
+        }
+        LinkedListNode<(string label, int f)> lens = list.First;
+        while (lens != null)
+        {
+            if (lens.Value.label == label)
+            {
+                break;
+            }
+            lens = lens.Next;
+        }
+        if (action == '=')
+        {
+            if (lens == null)
+            {
+                list.AddLast((label, f));
+            }
+            else
+            {
+                lens.Value = (label, f);
+            }
+        }
+        else
+        {
+            if (lens != null)
+            {
+                list.Remove(lens);
+            }
+        }
+    }
 }
 
-int GetGash(string x)
+total = boxes.SelectMany(b => b.Value.Select((lens, i) => (b.Key + 1) * (i + 1) * lens.f)).Sum();
+
+Console.WriteLine(total);
+Console.WriteLine("day15 completed.");
+
+static IEnumerable<string> TestInput() => GetLinesFromResource("day15.Input.TestInput.txt");
+static IEnumerable<string> PuzzleInput() => GetLinesFromResource("day15.Input.PuzzleInput.txt");
+
+static int GetHash(string x)
 {
     int current = 0;
 
@@ -17,12 +78,6 @@ int GetGash(string x)
 
     return current;
 }
-
-Console.WriteLine(total);
-Console.WriteLine("day15 completed.");
-
-static IEnumerable<string> TestInput() => GetLinesFromResource("day15.Input.TestInput.txt");
-static IEnumerable<string> PuzzleInput() => GetLinesFromResource("day15.Input.PuzzleInput.txt");
 
 static IEnumerable<string> GetLinesFromResource(string name)
 {
