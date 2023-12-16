acbe5800d22c4871d3d5cbf52e4c21a20fd91932
redeyens <54171005+redeyens@users.noreply.github.com>
2023-12-16 14:44:12 +0100

    2023 day 16 part 2

--- 2023/day16/Program.cs
+++ 2023/day16/Program.cs
@@ -1,44 +1,13 @@
 ﻿var input = PuzzleInput().ToList();
 var elements = input.SelectMany((line, row) => line.Select((c, col) => (c, row, col)).Where(x => x.c != '.')).ToList();
-var ray = (startRow: 0, startCol: int.MinValue, endRow: 0, endCol: int.MaxValue);
-var visitedRays = new HashSet<(int startRow, int startCol, int endRow, int endCol)>();
-var rays = new Queue<(int startRow, int startCol, int endRow, int endCol)>();
-rays.Enqueue(ray);
-var energized = new HashSet<(int row, int col)>();
+var possibleStartRays = Enumerable.Range(0, input.Count).Select(row => (startRow: 0, startCol: int.MinValue, endRow: 0, endCol: int.MaxValue))
+    .Concat(Enumerable.Range(0, input.Count).Select(row => (startRow: 0, startCol: int.MaxValue, endRow: 0, endCol: int.MinValue)))
+    .Concat(Enumerable.Range(0, input[0].Length).Select(col => (startRow: int.MinValue, startCol: col, endRow: int.MaxValue, endCol: col)))
+    .Concat(Enumerable.Range(0, input[0].Length).Select(col => (startRow: int.MaxValue, startCol: col, endRow: int.MinValue, endCol: col)));
 
-while (rays.Count > 0)
-{
-    var cR = rays.Dequeue();
-    visitedRays.Add(cR);
-    var possibleHit = elements.Where(x => x.row >= Math.Min(cR.startRow, cR.endRow) && x.row <= Math.Max(cR.startRow, cR.endRow) && x.col >= Math.Min(cR.startCol, cR.endCol) && x.col <= Math.Max(cR.startCol, cR.endCol));
-    possibleHit = (cR.endCol > cR.startCol) ? possibleHit.OrderBy(x => x.col).Where(x => x.c != '-' && x.col != cR.startCol)
-        : (cR.startCol > cR.endCol) ? possibleHit.OrderByDescending(x => x.col).Where(x => x.c != '-' && x.col != cR.startCol)
-        : (cR.endRow > cR.startRow) ? possibleHit.OrderBy(x => x.row).Where(x => x.c != '|' && x.row != cR.startRow)
-        : (cR.startRow > cR.endRow) ? possibleHit.OrderByDescending(x => x.row).Where(x => x.c != '|' && x.row != cR.startRow)
-        : throw new InvalidOperationException();
-    var hit = possibleHit.FirstOrDefault();
+int energy = possibleStartRays.Select(ray => GetEnergy(elements, ray, input.Count - 1, input[0].Length - 1)).Max();
 
-    if (hit.c != default(char))
-    {
-        cR.endRow = hit.row;
-        cR.endCol = hit.col;
-
-        foreach (var r in GetNextRays(cR, hit).Except(visitedRays))
-        {
-            rays.Enqueue(r);
-        }
-    }
-
-    for (int i = Math.Max(0, Math.Min(cR.startRow, cR.endRow)); i <= Math.Min(Math.Max(cR.startRow, cR.endRow), input.Count - 1); i++)
-    {
-        for (int j = Math.Max(0, Math.Min(cR.startCol, cR.endCol)); j <= Math.Min(Math.Max(cR.startCol, cR.endCol), input[0].Length - 1); j++)
-        {
-            energized.Add((i, j));
-        }
-    }
-}
-
-Console.WriteLine(energized.Count);
+Console.WriteLine(energy);
 Console.WriteLine("day16 completed.");
 
 static IEnumerable<string> TestInput() => GetLinesFromResource("day16.Input.TestInput.txt");
@@ -106,4 +75,47 @@ static IEnumerable<string> GetLinesFromResource(string name)
     {
         yield return line;
     }
+}
+
+static int GetEnergy(List<(char c, int row, int col)> elements, (int startRow, int startCol, int endRow, int endCol) ray, int maxRow, int maxCol)
+{
+    var visitedRays = new HashSet<(int startRow, int startCol, int endRow, int endCol)>();
+    var rays = new Queue<(int startRow, int startCol, int endRow, int endCol)>();
+    rays.Enqueue(ray);
+    var energized = new HashSet<(int row, int col)>();
+
+    while (rays.Count > 0)
+    {
+        var cR = rays.Dequeue();
+        visitedRays.Add(cR);
+        var possibleHit = elements.Where(x => x.row >= Math.Min(cR.startRow, cR.endRow) && x.row <= Math.Max(cR.startRow, cR.endRow) && x.col >= Math.Min(cR.startCol, cR.endCol) && x.col <= Math.Max(cR.startCol, cR.endCol));
+        possibleHit = (cR.endCol > cR.startCol) ? possibleHit.OrderBy(x => x.col).Where(x => x.c != '-' && x.col != cR.startCol)
+            : (cR.startCol > cR.endCol) ? possibleHit.OrderByDescending(x => x.col).Where(x => x.c != '-' && x.col != cR.startCol)
+            : (cR.endRow > cR.startRow) ? possibleHit.OrderBy(x => x.row).Where(x => x.c != '|' && x.row != cR.startRow)
+            : (cR.startRow > cR.endRow) ? possibleHit.OrderByDescending(x => x.row).Where(x => x.c != '|' && x.row != cR.startRow)
+            : throw new InvalidOperationException();
+        var hit = possibleHit.FirstOrDefault();
+
+        if (hit.c != default(char))
+        {
+            cR.endRow = hit.row;
+            cR.endCol = hit.col;
+
+            foreach (var r in GetNextRays(cR, hit).Except(visitedRays))
+            {
+                rays.Enqueue(r);
+            }
+        }
+
+        for (int i = Math.Max(0, Math.Min(cR.startRow, cR.endRow)); i <= Math.Min(Math.Max(cR.startRow, cR.endRow), maxRow); i++)
+        {
+            for (int j = Math.Max(0, Math.Min(cR.startCol, cR.endCol)); j <= Math.Min(Math.Max(cR.startCol, cR.endCol), maxCol); j++)
+            {
+                energized.Add((i, j));
+            }
+        }
+    }
+
+    var energy = energized.Count;
+    return energy;
 }
