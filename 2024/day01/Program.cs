0d1493e430d0f3e75805ce937eb2dea4b953c7dd
redeyens <54171005+redeyens@users.noreply.github.com>
2024-12-02 22:36:18 +0100

    2024 day 01 part 1

---
+++ 2024/day01/Program.cs
@@ -0,0 +1,31 @@
+﻿List<long> left = new List<long>();
+List<long> right = new List<long>();
+foreach (string line in PuzzleInput())
+{
+    var locations = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+    left.Add(long.Parse(locations[0]));
+
+    right.Add(long.Parse(locations[1]));
+}
+
+var res = left.OrderBy(x => x).Zip(right.OrderBy(x => x), (l, r) => Math.Abs(l - r)).Sum();
+
+System.Console.WriteLine(res);
+
+Console.WriteLine("day01 completed.");
+
+static IEnumerable<string> TestInput() => GetLinesFromResource("day01.Input.TestInput.txt");
+static IEnumerable<string> PuzzleInput() => GetLinesFromResource("day01.Input.PuzzleInput.txt");
+
+static IEnumerable<string> GetLinesFromResource(string name)
+{
+    using Stream inStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(name) ?? throw new InvalidOperationException("Could not open input stream!");
+    using TextReader inReader = new StreamReader(inStream);
+
+    string? line;
+    while ((line = inReader.ReadLine()) != null)
+    {
+        yield return line;
+    }
+}
