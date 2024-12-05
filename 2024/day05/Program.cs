3f768df23903995a2afa60bca2a69cd5531531f2
redeyens <54171005+redeyens@users.noreply.github.com>
2024-12-05 07:38:45 +0100

    2024 day 05 part 2

--- 2024/day05/Program.cs
+++ 2024/day05/Program.cs
@@ -8,7 +8,7 @@ var res = 0;
 foreach (var manual in parts[1].Split(Environment.NewLine))
 {
     var pages = manual.Split(",");
-    if (IsManualValid(rules, pages))
+    if (IsManualCorrected(rules, pages))
     {
         res += int.Parse(pages[pages.Length / 2]);
     }
@@ -33,19 +33,28 @@ static IEnumerable<string> GetLinesFromResource(string name)
     }
 }
 
-static bool IsManualValid(HashSet<string> rules, string[] pages)
+static bool IsManualCorrected(HashSet<string> rules, string[] pages)
 {
+    var wasCorrected = false;
     for (int i = 0; i < pages.Length - 1; i++)
     {
+        var inversionPerformed = false;
         for (int j = i + 1; j < pages.Length; j++)
         {
             var orderedPair = string.Join("|", pages[j], pages[i]);
             if (rules.Contains(orderedPair))
             {
-                return false;
+                (pages[i], pages[j]) = (pages[j], pages[i]);
+                wasCorrected = true;
+                inversionPerformed = true;
+                break;
             }
+        }
+        if (inversionPerformed)
+        {
+            i--;
         }
     }
 
-    return true;
+    return wasCorrected;
 }
