a614034c3e5462a7e3a742a5c0da873d8b4e564b
redeyens <54171005+redeyens@users.noreply.github.com>
2024-12-06 23:42:17 +0100

    2024 day 06 part 2

--- 2024/day06/Program.cs
+++ 2024/day06/Program.cs
@@ -3,10 +3,10 @@
 var obstacleMatcher = new Regex("#");
 var row = 0;
 var obstacles = new HashSet<(int x, int y)>();
-(int x, int y) guardPos = default;
+(int x, int y) startingPos = default;
 int maxX = 0;
 int maxY = 0;
-(int dx, int dy) move = (0, -1);
+(int dx, int dy) initialDirection = (0, -1);
 
 foreach (string line in PuzzleInput())
 {
@@ -14,7 +14,7 @@ foreach (string line in PuzzleInput())
     var guardCol = line.IndexOf('^');
     if (guardCol > -1)
     {
-        guardPos = (x: guardCol, y: row);
+        startingPos = (x: guardCol, y: row);
     }
     foreach (var obstacle in obstacleMatcher.Matches(line).Select(m => (x: m.Index, y: row)))
     {
@@ -24,24 +24,50 @@ foreach (string line in PuzzleInput())
 }
 maxY = row;
 
-var visitedLocations = new HashSet<(int x, int y)>();
-while (guardPos.x >= 0 && guardPos.x < maxX && guardPos.y >= 0 && guardPos.y < maxY)
+var possibleObstacles = new HashSet<(int x, int y)>();
+var originalPath = GetPath(startingPos, initialDirection);
+
+foreach (var (pos, dir) in originalPath)
 {
-    visitedLocations.Add(guardPos);
-    var newPos = (x: guardPos.x + move.dx, y: guardPos.y + move.dy);
-    if (obstacles.Contains(newPos))
+    obstacles.Add(pos);
+
+    var testPath = GetPath(startingPos, initialDirection);
+
+    if (!testPath.Select(x => (x: x.pos.x + x.dir.dx, y: x.pos.y + x.dir.dy)).Any(p => !IsInside(maxX, maxY, p)))
     {
-        move = TurnRight(move);
+        possibleObstacles.Add(pos);
     }
-    else
-    {
-        guardPos = newPos;
-    }
+
+    obstacles.Remove(pos);
 }
 
-System.Console.WriteLine(visitedLocations.Count);
+Console.WriteLine(possibleObstacles.Count);
 
 Console.WriteLine("day06 completed.");
+
+HashSet<((int x, int y) pos, (int dx, int dy) dir)> GetPath((int x, int y) startingPos, (int dx, int dy) initialDirection)
+{
+    var currentPos = startingPos;
+    var move = initialDirection;
+    var path = new HashSet<((int x, int y) pos, (int dx, int dy) dir)>();
+    while (IsInside(maxX, maxY, currentPos))
+    {
+        if (!path.Add((currentPos, move)))
+        {
+            break;
+        }
+        var newPos = (x: currentPos.x + move.dx, y: currentPos.y + move.dy);
+        if (obstacles.Contains(newPos))
+        {
+            move = TurnRight(move);
+        }
+        else
+        {
+            currentPos = newPos;
+        }
+    }
+    return path;
+}
 
 (int dx, int dy) TurnRight((int dx, int dy) move) => move switch
 {
@@ -65,4 +91,9 @@ static IEnumerable<string> GetLinesFromResource(string name)
     {
         yield return line;
     }
+}
+
+static bool IsInside(int maxX, int maxY, (int x, int y) currentPos)
+{
+    return currentPos.x >= 0 && currentPos.x < maxX && currentPos.y >= 0 && currentPos.y < maxY;
 }
