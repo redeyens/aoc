6a9b0f0051214ae41d607f476e55e1bb5d277dda
redeyens <54171005+redeyens@users.noreply.github.com>
2024-12-12 15:12:50 +0100

    2024 day 12 part 2

--- 2024/day12/Program.cs
+++ 2024/day12/Program.cs
@@ -1,17 +1,19 @@
-﻿var map = PuzzleInput().Select(line => line.ToCharArray()).ToArray();
+﻿using System.Data;
 
-var perimeter = new Queue<(int row, int col)>();
+var map = PuzzleInput().Select(line => line.ToCharArray()).ToArray();
+
+var perimeter = new Queue<(int row, int col, int dir)>();
 var outerPerimeter = new Queue<(int row, int col)>();
 var visited = new HashSet<(int row, int col)>();
-var regions = new List<(char color, int area, int perimeter)>();
+var regions = new List<(char color, int area, List<(int row, int col, int dir)> perimeter)>();
 
 outerPerimeter.Enqueue((0, 0));
 
 while (outerPerimeter.Count > 0)
 {
     var startingPos = outerPerimeter.Dequeue();
-    var currentRegion = (color: map[startingPos.row][startingPos.col], area: 0, perimeter: 0);
-    perimeter.Enqueue(startingPos);
+    var currentRegion = (color: map[startingPos.row][startingPos.col], area: 0, perimeter: new List<(int row, int col, int dir)>());
+    perimeter.Enqueue((startingPos.row, startingPos.col, 0));
 
     while (perimeter.Count > 0)
     {
@@ -19,7 +21,7 @@ while (outerPerimeter.Count > 0)
 
         if (currentRegion.color == map[current.row][current.col])
         {
-            if (visited.Contains(current))
+            if (visited.Contains((current.row, current.col)))
             {
                 continue;
             }
@@ -28,46 +30,46 @@ while (outerPerimeter.Count > 0)
 
             if (current.row == 0)
             {
-                currentRegion.perimeter += 1;
+                currentRegion.perimeter.Add((current.row - 1, current.col, 0));
             }
             else
             {
-                perimeter.Enqueue((current.row - 1, current.col));
+                perimeter.Enqueue((current.row - 1, current.col, 0));
             }
 
             if (current.col == 0)
             {
-                currentRegion.perimeter += 1;
+                currentRegion.perimeter.Add((current.row, current.col - 1, 1));
             }
             else
             {
-                perimeter.Enqueue((current.row, current.col - 1));
+                perimeter.Enqueue((current.row, current.col - 1, 1));
             }
 
             if (current.row == map.Length - 1)
             {
-                currentRegion.perimeter += 1;
+                currentRegion.perimeter.Add((current.row + 1, current.col, 2));
             }
             else
             {
-                perimeter.Enqueue((current.row + 1, current.col));
+                perimeter.Enqueue((current.row + 1, current.col, 2));
             }
 
             if (current.col == map[current.row].Length - 1)
             {
-                currentRegion.perimeter += 1;
+                currentRegion.perimeter.Add((current.row, current.col + 1, 3));
             }
             else
             {
-                perimeter.Enqueue((current.row, current.col + 1));
+                perimeter.Enqueue((current.row, current.col + 1, 3));
             }
 
-            visited.Add(current);
+            visited.Add((current.row, current.col));
         }
         else
         {
-            outerPerimeter.Enqueue(current);
-            currentRegion.perimeter += 1;
+            outerPerimeter.Enqueue((current.row, current.col));
+            currentRegion.perimeter.Add(current);
         }
     }
 
@@ -77,9 +79,25 @@ while (outerPerimeter.Count > 0)
     }
 }
 
-Console.WriteLine(regions.Sum(r => r.area * r.perimeter));
+Console.WriteLine(regions.Sum(r => r.area * CountSides(r)));
 
 Console.WriteLine("day12 completed.");
+
+int CountSides((char color, int area, List<(int row, int col, int dir)> perimeter) area)
+{
+    var a = area.perimeter.GroupBy(x => (x.row, x.dir)).ToDictionary(x => x.Key, y => y.ToList());
+    var b = a.Values.Select(x => x.Where(a => a.dir == 0 || a.dir == 2))
+        .Where(x => x.Any()).Select(x => x.ToList()).ToList();
+    var horizontal = b.Select(x => x.OrderBy(a => a.col).Zip(x.OrderBy(b => b.col).Skip(1)).Count(x => (x.Second.col - x.First.col) > 1) + 1).Sum();
+    var d = area.perimeter.GroupBy(x => (x.col, x.dir)).ToDictionary(x => x.Key, y => y.ToList());
+    var c = d.Values
+        .Select(g => g.Where(a => a.dir == 1 || a.dir == 3).ToList())
+        .Where(x => x.Any()).ToList();
+    var vertical = c
+        .Select(x => x.OrderBy(a => a.row).Zip(x.OrderBy(b => b.row).Skip(1)).Count(x => (x.Second.row - x.First.row) > 1) + 1).Sum();
+
+    return horizontal + vertical;
+}
 
 static IEnumerable<string> TestInput() => GetLinesFromResource("day12.Input.TestInput.txt");
 static IEnumerable<string> PuzzleInput() => GetLinesFromResource("day12.Input.PuzzleInput.txt");
